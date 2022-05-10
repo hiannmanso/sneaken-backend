e71d4fb166f3574a1f9ae215c16b51b2034d3494
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-10 20:32:25 -0300

    teste heroku

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("app no ar");
+});
 app.listen(process.env.PORT);
