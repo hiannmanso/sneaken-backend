339bd779bb8d61f9e066fa9094ff7e7f3fbe2bbc
Hiann <hiannmanso@email.com>
2022-05-11 19:06:03 -0300

    teste

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import express from "express";
+import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.listen(process.env.PORT);
+
+app.listen(process.env.PORT, () => {
+	console.log('Backend aberto na porta 5000!');
+});
