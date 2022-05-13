b2723f2a6692118d6d326987b1ba9387733587af
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-13 12:01:07 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
+import cors from 'cors';
 import productsRouter from './Routers/productsRouter.js';
 import signInRouter from './Routers/signInRouter.js';
 import signUpRouter from './Routers/signUpRouter.js';
@@ -8,6 +9,7 @@ import signUpRouter from './Routers/signUpRouter.js';
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(signUpRouter);
 app.use(signInRouter);
 app.get('/', (req, res) => {
