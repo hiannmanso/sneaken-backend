a84d8c24b307bc517023714c47b09ac58f102fcd
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-12 23:10:18 -0300

    validation middleware

--- Controllers/productsController.js
+++ Controllers/productsController.js
@@ -2,19 +2,11 @@ import express from 'express';
 import database from '../database.js';
 import Joi from 'joi';
 
-let itemSchema = Joi.object({
-    brand: Joi.string().min(1).required(),
-    model: Joi.string().min(1).required(),
-    amount: Joi.number().required(),
-    price: Joi.string().min(1).required() 
-}); 
-
 export async function productsPOST(req, res){
     let item = req.body;
-    let { error } = itemSchema.validate(item);
     const { authorization } = req.headers;
 	const token = authorization?.replace('Bearer', '').trim();
-    if(error === undefined && token){
+    if(token){
         try{
             let access = await database.collection('session').find({token: token}).toArray();
             if(access.length !== 0){
--- Routers/productsRouter.js
+++ Routers/productsRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { productsGET, productsPOST } from '../Controllers/productsController.js';
+import { validProducts } from '../Middlewares/validProducts.js';
 
 const productsRouter = express.Router();
-productsRouter.post('/products', productsPOST);
+productsRouter.post('/products', validProducts, productsPOST);
 productsRouter.get('/products', productsGET);
 
 export default productsRouter;
