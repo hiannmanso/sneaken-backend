9a180eb4eaedbbd7ebcffad10c887d789a13fb84
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-13 23:23:24 -0300

    new products object with sizes

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ import dotenv from 'dotenv';
 import chalk from 'chalk';
 import cors from 'cors';
 
-import productsRouter from './Routers/productsRouter.js';
 import signInRouter from './Routers/signInRouter.js';
 import signUpRouter from './Routers/signUpRouter.js';
 import mySneakersRouter from './Routers/mySneakersRouter.js';
+import productsRouter from './Routers/productsRouter.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(cors());
 app.use(signUpRouter);
 app.use(signInRouter);
-app.use(productsRouter);
 app.use(mySneakersRouter);
+app.use(productsRouter);
 
 app.listen(process.env.PORT, () => {
 	console.log(
