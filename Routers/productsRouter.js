921dfb80a3b29fd6e58a939ee2bde27e79168c7d
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-14 19:36:14 -0300

    get only one product

--- Routers/productsRouter.js
+++ Routers/productsRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { productsGET, productsPOST } from '../Controllers/productsController.js';
+import { productGET, productsGET, productsPOST } from '../Controllers/productsController.js';
 import { validProducts } from '../Middlewares/validProducts.js';
 
 const productsRouter = express.Router();
 productsRouter.post('/products', validProducts, productsPOST);
 productsRouter.get('/products', productsGET);
+productsRouter.get('/products/:product', productGET);
 export default productsRouter;
